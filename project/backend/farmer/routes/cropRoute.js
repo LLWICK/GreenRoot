71c35899277891a511b05667fa8be4e16ae180c0
LLWICK <linwick679@gmail.com>
2025-03-09 14:14:32 +0530

    crop management part1

---
+++ project/backend/farmer/routes/cropRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const Route = express.Router();
+
+const {
+  allCrops,
+  findbyId,
+  insertCrop,
+  updateCrop,
+  deleteCrop,
+} = require("../controller/cropController");
+
+Route.get("/", allCrops);
+Route.get("/:id", findbyId);
+Route.post("/", insertCrop);
+Route.patch("/:id", updateCrop);
+Route.delete("/:id", deleteCrop);
+
+module.exports = Route;
