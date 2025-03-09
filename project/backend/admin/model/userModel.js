e969c45db19e2213df79f942fcd12970d6ff4408
Prasa-Xma <prasadbandara130@gmail.com>
2025-03-09 10:09:38 +0530

    add user model schema

---
+++ project/backend/admin/model/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongodb');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['admin', 'farmer', 'researcher', 'seller', 'customer']
+    },
+
+});
