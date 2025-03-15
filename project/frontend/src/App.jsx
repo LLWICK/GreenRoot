e783c6fe329e09bb82ac97c9cbf6b4b8d6b93f56
LLWICK <linwick679@gmail.com>
2025-03-15 09:30:23 +0530

    Add sidebar design
    Add sidebar to the Common folder please copy the code into your respective folder and modify it

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { Route, Routes } from "react-router-dom";
+import Sidebar from "./Common/Sidebar";
 
 function App() {
   return (
     <Routes>
-      <Route />
+      <Route path="/test" element={<Sidebar />} />
     </Routes>
   );
 }
