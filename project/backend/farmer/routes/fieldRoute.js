184a9d5d0a9fb33ae8ed9b9fea4b3aa49bab3b1f
LLWICK <linwick679@gmail.com>
2025-03-19 17:51:53 +0530

    Implement fetch map function

--- project/backend/farmer/routes/fieldRoute.js
+++ project/backend/farmer/routes/fieldRoute.js
@@ -14,6 +14,7 @@ Route.get("/", allFields);
 Route.get("/:id", fieldbyId);
 Route.post("/parameters", fieldsByParams);
 Route.post("/", insertfield);
+
 Route.patch("/:id", updateField);
 Route.delete("/:id", deleteField);
 
