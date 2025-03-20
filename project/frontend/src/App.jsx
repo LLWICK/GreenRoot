b0fd7056f1d8207c096b9c96c805921733e3f4df
Prasa-Xma <prasadbandara130@gmail.com>
2025-03-20 06:32:53 +0530

    add SweetAlert2 dependency and update admin dashboard route

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       <Route path="/auth/login" element={<LoginPage />} />
 
       {/* Admin Routers */}
-      <Route path="/admin/dashboard" element={<AdminDashbord />} />
+      <Route path="/admin/:id/dashboard" element={<AdminDashbord />} />
       <Route path="/admin/user-management" element={<UserManagement />} />
 
       <Route path="/admin/user-management/farmer" element={<FarmerManagement />} />
