e0266c7e2e675ef7435f650d79d1946b93993fa3
LLWICK <linwick679@gmail.com>
2025-03-25 10:13:40 +0530

    Update Landing page

--- project/frontend/src/admin/pages/home/Landing.jsx
+++ project/frontend/src/admin/pages/home/Landing.jsx
@@ -14,7 +14,7 @@ const Landing = () => {
             muted
             loop
           ></video>
-          <div className="absolute inset-0  bg-opacity-60"></div>
+          <div className="absolute inset-0  bg-opacity-60 transition-opacity duration-500"></div>
         </div>
         <div className="z-10 space-y-2">
           <h1 className="font-light text-6xl">We Provide Landscaping</h1>
@@ -31,7 +31,19 @@ const Landing = () => {
             </a>
             <a
               href="#"
-              className="px-6 py-2 border border-green-500 text-green-500 rounded-lg hover:bg-green-500 hover:text-white transition"
+              className="px-6 py-2 border border-green-500 text-green-500 rounded-lg hover:bg-green-500 hover:text-white transition relative"
+              onMouseEnter={(e) =>
+                e.currentTarget
+                  .closest("section")
+                  .querySelector("div.absolute.inset-0")
+                  .classList.add("bg-opacity-100")
+              }
+              onMouseLeave={(e) =>
+                e.currentTarget
+                  .closest("section")
+                  .querySelector("div.absolute.inset-0")
+                  .classList.remove("bg-opacity-80")
+              }
             >
               Contact Us
             </a>
