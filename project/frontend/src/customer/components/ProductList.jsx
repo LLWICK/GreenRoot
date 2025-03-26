995269b3167a90d836da7e9e1df277d083524cb6
LLWICK <linwick679@gmail.com>
2025-03-26 08:46:28 +0530

    update products

--- project/frontend/src/customer/components/ProductList.jsx
+++ project/frontend/src/customer/components/ProductList.jsx
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import ProductItem from './ProductItem';
+import React, { useState, useEffect } from "react";
+import ProductItem from "./ProductItem";
 
 const ProductList = () => {
   const [products, setProducts] = useState(null);
 
-  useEffect(()=>{
-    const fetchProducts = async()=>{
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/api/RetailSeller/products/products"
+        ); //change
+        // const json = await response.json()
 
-     try {
-     const response = await fetch('http://localhost:3000/api/RetailSeller/products/products')//change
-     // const json = await response.json()
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        setProducts(json.products);
+        console.log(json.products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
 
-     if(!response.ok){
-       throw new Error(`HTTP error! status: ${response.status}`);
-     }
-     const json = await response.json()
-     setProducts(json.products);
-           console.log(json.products); 
-         } catch (error) {
-           console.error("Error fetching products:", error);
-       }
- }
-
- fetchProducts()
-}
- ,[])
+    fetchProducts();
+  }, []);
 
   return (
     <div className="mt-10">
-      <h2 className="text-green-600 font-bold text-2xl">Our Popular products</h2>
+      <h2 className="text-green-600 font-bold text-2xl">
+        Our Popular products
+      </h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-6">
         {products &&
           products.length > 0 &&
-          products.map((product,index) => index<8 &&(
-            <ProductItem key={product._id} product={product} />
-          ))}
+          products.map(
+            (product, index) =>
+              index < 8 && <ProductItem key={product._id} product={product} />
+          )}
       </div>
     </div>
   );
