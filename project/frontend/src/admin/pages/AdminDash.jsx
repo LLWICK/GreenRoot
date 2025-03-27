78ccf6cdd0f43b3edc04729125fa483185d4fc62
Prasa-Xma <prasadbandara130@gmail.com>
2025-03-27 17:29:14 +0530

    create user chart

--- project/frontend/src/admin/pages/AdminDash.jsx
+++ project/frontend/src/admin/pages/AdminDash.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
-import Sidebar from '../components/Sidebar';
+// import Sidebar from '../components/Sidebar';
 import { Link } from 'react-router-dom';
 import NavBar from './home/home_components/NavBar';
+import UserRoleChart from '../components/UserRoleChart';
 // import Footer from './home/home_components/Footer';
-// import AdminSideBar from '../components/AdminSideBar';
+
 
 function AdminDash() {
     // const navigate = useNavigate();
 
     return (
-        <>
+        <div>
             <>
                 <NavBar />
             </>
-            {/* <AdminSideBar /> */}
-            <Sidebar />
+
+            <div className='px-6'><h1 className='text-3xl my-4'>Admin Dashboard</h1></div>
+
+            {/* user role chart */}
+            <div className='flex justify-center items-center'>
+                <div className='container p-4'>
+                    <UserRoleChart />
+                </div>
+            </div>
+
+
             <>
                 <div className='p-4'>
-                    <h1 className='text-3xl my-4'>Admin Dashboard</h1>
+
                     <button
                         className='bg-blue-600 text-white px-4 py-2 rounded-md'
 
@@ -30,7 +40,7 @@ function AdminDash() {
                 </div>
             </>
             {/* <><Footer /></> */}
-        </>
+        </div>
     )
 }
 
