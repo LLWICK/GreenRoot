a197246d53001c2fbb5a0645dbcc80c399c12e85
Matheesha-w <ggmatheesha@gmail.com>
2025-03-29 19:03:53 +0530

    matheesha
    sidebar edited,dashborad updated,hom updated

--- project/frontend/src/customer/components/Sidebar.jsx
+++ project/frontend/src/customer/components/Sidebar.jsx
@@ -49,7 +49,7 @@ function Sidebar() {
             className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out flex items-center"
           >
             <Package className="w-4 h-4 mr-2" />
-            <span>Crops</span>
+            <span>Products</span>
           </Link>
           <Link
             to="/Customer/Orderhistory" // Add link to orders page
--- project/frontend/src/customer/pages/DashboardPage.jsx
+++ project/frontend/src/customer/pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar'; // Import the Sidebar component
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Footer from '@/admin/pages/home/home_components/Footer';
 
 const DashboardPage = () => {
     return (
