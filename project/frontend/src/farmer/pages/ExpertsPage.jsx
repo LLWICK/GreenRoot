6aaafbd9bb0ef1a7f3d08e8efba3577e8b44b7ec
LLWICK <linwick679@gmail.com>
2025-04-23 03:13:58 +0530

    ticket frontend

--- project/frontend/src/farmer/pages/ExpertsPage.jsx
+++ project/frontend/src/farmer/pages/ExpertsPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import QADashboard from "../components/QADashboard";
 import NavBFarmer from "../extras/NavBFarmer";
+import TicketTable from "../QADashboard/TicketTable";
 
 function ExpertsPage() {
   return (
     <div>
       <NavBFarmer />
       <Sidebar />
-      <QADashboard />
+      <TicketTable />
     </div>
   );
 }
