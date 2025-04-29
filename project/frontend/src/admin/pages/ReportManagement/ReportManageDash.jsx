43f2ffadf22ee56d07a22c87e4bf247e43637822
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-29 12:08:52 +0530

    layout the report page

--- project/frontend/src/admin/pages/ReportManagement/ReportManageDash.jsx
+++ project/frontend/src/admin/pages/ReportManagement/ReportManageDash.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from '../../components/Sidebar';
+import BackButton from '../../components/BackButton';
 
 const ReportManageDash = () => {
     return (
-        <div>Report Management</div>
+        <>
+            <div className='flex h-screen'>
+                {/* SideBar */}
+                <Sidebar />
+
+                <div className="flex-1 p-6 ml-20">
+                    <div className="mb-4">
+                        <BackButton />
+                    </div>
+
+                    {/* Content */}
+                    <div className="bg-white shadow-lg rounded-lg p-6">
+                        <h2 className="text-2xl font-semibold mb-4">Report Management</h2>
+
+                    </div>
+                </div>
+
+            </div>
+        </>
     )
 }
 
