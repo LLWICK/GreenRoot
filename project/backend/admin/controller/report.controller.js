45d4710c0fd5b20903835167fd6b62400afdb421
Prasa-Xma <prasadbandara130@gmail.com>
2025-05-08 11:44:35 +0530

    update report controller

--- project/backend/admin/controller/report.controller.js
+++ project/backend/admin/controller/report.controller.js
@@ -3,13 +3,13 @@ const XLSX = require('xlsx');
 
 const getAllQuestionsForExcel = async (req, res) => {
     try {
-        const questions = await Question.find();
+        const questions = await Question.find().populate('createdBy', 'firstName lastName email').lean();
 
-        const formatted = getAllQuestionsForExcel.map(q => ({
+        const formatted = questions.map(q => ({
             Title: q.title,
             Custom_Title: q.customTitle || '',
             Message: q.message,
-            Created_By: q.createdBy,
+            Created_By: `${q.createdBy?.firstName || ''} ${q.createdBy?.lastName || ''}`,
             Email: q.createdBy?.email || '',
             Created_At: new Date(q.createdAt).toLocaleString(),
             Is_Closed: q.isClosed ? 'Yes' : 'No',
@@ -18,16 +18,17 @@ const getAllQuestionsForExcel = async (req, res) => {
 
         const worksheet = XLSX.utils.json_to_sheet(formatted);
         const workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, worksheet, 'Questions')
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Questions');
 
         const buffer = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
-        res.setHeader('Content-Disposition', 'attachement; filename="questions.xlsx"');
+
+        res.setHeader('Content-Disposition', 'attachment; filename="questions.xlsx"');
         res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         res.send(buffer);
 
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: `Failed` })
+        res.status(500).json({ message: 'Failed' });
     }
 };
 
