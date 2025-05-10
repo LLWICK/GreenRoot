62ab0610df28898e6dc02d8ae44ac124a742e032
LLWICK <linwick679@gmail.com>
2025-05-10 21:33:20 +0530

    update

--- project/backend/farmer/routes/chatRouts.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Route = express.Router();
-
-const { chatPost } = require("../controller/chatController");
-
-Route.post("/", chatPost);
-
-module.exports = Route;
