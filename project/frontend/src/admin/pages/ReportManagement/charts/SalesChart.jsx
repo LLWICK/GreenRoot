cade6893121befeb11a89d51396a8bf978e3e081
Prasa-Xma <prasadbandara130@gmail.com>
2025-05-12 16:54:01 +0530

    update

--- project/frontend/src/admin/pages/ReportManagement/charts/SalesChart.jsx
+++ project/frontend/src/admin/pages/ReportManagement/charts/SalesChart.jsx
@@ -90,9 +90,9 @@ const SalesChart = () => {
 
                     <button
                         onClick={downloadExcel}
-                        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                        className="mt-4 px-4 ml-1.5 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
                     >
-                        Download Image
+                        Download Excel
                     </button>
                 </div>
 
