2661eeed4b2fb3c21e3f505c950f37995c4559c2
LLWICK <linwick679@gmail.com>
2025-05-13 11:24:01 +0530

    update crop element set warning

--- project/frontend/src/farmer/components/CropTable.jsx
+++ project/frontend/src/farmer/components/CropTable.jsx
@@ -47,7 +47,7 @@ function CropTable(prop) {
             href="https://www.material-tailwind.com/docs/html/table"
             class="block w-full px-4 py-2 text-center text-slate-700 transition-all "
           >
-            More components on <b>Material Tailwind</b>.
+            On field crops <b>Ready to go!</b>.
           </a>
         </div>
 
@@ -257,7 +257,7 @@ function CropTable(prop) {
                         <div class="w-max">
                           <Link
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                            to={`/farmer/viewCrop/${element._id}`}
+                            to={`/farmer/${prop.fid}/viewCrop/${element._id}`}
                           >
                             View
                           </Link>
